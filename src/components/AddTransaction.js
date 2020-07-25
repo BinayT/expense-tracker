d32fe9592c586a5839d017420d9a91ab5071206b
Binay <binayprogramming@gmail.com>
2020-07-25 21:40:47 +0200

    Adding transactions works as well

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState("");
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000000),
+      text,
+      amount: amount * 1,
+    };
+    addTransaction(newTransaction);
+    setText("");
+    setAmount("");
+  };
   return (
     <div>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
@@ -28,7 +44,9 @@ export const AddTransaction = () => {
             onChange={(e) => setAmount(e.target.value)}
           />
         </div>
-        <button className="btn">Add transaction</button>
+        <button className="btn" type="submit">
+          Add transaction
+        </button>
       </form>
     </div>
   );
