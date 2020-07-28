80e31637c914aa12ac1e39152a76211bd634e386
Binay <binayprogramming@gmail.com>
2020-07-29 00:28:09 +0200

    New methods added for CRUD operations

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -2,5 +2,19 @@
 // @router -- /api/v1/transactions
 // @access -- public
 exports.getTransactions = (req, res, next) => {
-  res.send("GET TRANSACTIONS");
+  res.send("GET Transactions");
+};
+
+// @desc -- ADDS TRANSACTION
+// @router -- /api/v1/transactions
+// @access -- public
+exports.addTransaction = (req, res, next) => {
+  res.send("POST Transaction");
+};
+
+// @desc -- DELETES TRANSACTION
+// @router -- /api/v1/transactions:id
+// @access -- public
+exports.deleteTransaction = (req, res, next) => {
+  res.send("DELETE Transaction");
 };
